aa1dbd448773537a17af65935ec4609666079f33
ls2947727081 <2947727081@qq.com>
2025-10-17 14:09:36 +0800

    添加jni_onload

--- app/src/main/cpp/MyHook/myhook.cpp
+++ app/src/main/cpp/MyHook/myhook.cpp
@@ -5,8 +5,21 @@ Java_com_hookeasy_liuhookworld_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+// HOOK JNI_OnLoad
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     if (shadowhook_init(SHADOWHOOK_MODE_UNIQUE, false) != 0) {
         LOGE("shadowhook_init failed: %d", shadowhook_get_errno());
+        return JNI_ERR;
     }
-    return env->NewStringUTF(hello.c_str());
+
+    JNIEnv* env = nullptr;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK || !env) {
+        LOGE("GetEnv failed");
+        return JNI_ERR;
+    }
+
+    return JNI_VERSION_1_6;
 }
